3e1f1036e6e5f31952edd9afa2b2ad9b1c881121
youngsterjaidev <47059807+youngsterjaidev@users.noreply.github.com>
2021-10-02 19:10:33 +0530

    Add the Base files

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,81 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 import Item from "./Item";
 
 interface Item {
   itemName: string;
   quantity: number;
+  price: number;
+  id: string;
 }
 
+const Container = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+`;
+
+const Heading = styled.h2`
+  text-align: center;
+`;
+
+const Checkout = styled.div`
+  position: sticky;
+  top: 0;
+  background-color: #fff;
+  box-shadow: 0px 0px 14px 1px #9898989c;
+  padding: 1em;
+`;
+
+const CheckoutItems = styled.div`
+  height: 60vh;
+  width: 25em;
+  overflow-y: auto;
+  background: #fff;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 1em;
+  border: none;
+  background-color: #000;
+  color: #fff;
+  margin: 1em 0em;
+  border-radius: 5px;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  padding: 0.5em 2em;
+  justify-content: space-between;
+`;
+
+const styles = {
+  heading: {
+    fontWeight: 600
+  },
+  pad: {
+    padding: "1em"
+  }
+};
+
 export default function App() {
-  const [menu, setMenu] = useState<any[]>([
-    {
-      itemName: "Burger",
-      quantity: 0
-    }
-  ]);
-  const [totalItem, setTotalItem] = useState<Item[] | null>([]);
+  const [menu, setMenu] = useState<Item[] | null>(null);
+  const [totalItem, setTotalItem] = useState<Item[] | null>(null);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   const changeItem = (itemName: string, quantity: number) => {
     let temp = menu.map((item) => {
       if (item.itemName !== itemName) return item;
       return {
         itemName: item.itemName,
-        quantity
+        quantity,
+        price: item.price,
+        id: item.id
       };
     });
     setMenu(temp);
@@ -31,41 +85,74 @@ export default function App() {
     // call the server of the menu
     setMenu([
       {
+        id: "1",
         itemName: "Tea",
-        quantity: 0
+        quantity: 0,
+        price: 100
       },
-      { itemName: "coffee", quantity: 0 }
+      { itemName: "coffee", quantity: 0, price: 150, id: "2" }
     ]);
   }, []);
 
+  function handlePay() {
+    console.log({
+      totalPrice,
+      items: totalItem
+    });
+  }
+
   useEffect(() => {
-    setTotalItem(menu.filter((item) => item.quantity > 0));
+    if (menu) {
+      setTotalItem(menu.filter((item) => item.quantity > 0));
+    }
   }, [menu]);
 
   useEffect(() => {
     console.log("state Update");
   }, [totalItem]);
 
-  return (
-    <div>
-      <h2>Start editing to see some magic happen!</h2>
-      <div>
-        {menu.map((item) => {
-          return <Item {...item} changeItem={changeItem} />;
-        })}
-      </div>
+  if (!menu) {
+    return <div>Loading</div>;
+  }
 
-      <div>
-        Okay
-        {totalItem?.map((item) => {
-          console.log(item);
-          return (
-            <div>
-              Item {item.itemName} - {item.quantity}
-            </div>
-          );
-        })}
-      </div>
-    </div>
+  return (
+    <>
+      <Heading>Menu</Heading>
+      <Container>
+        <div style={styles.pad}>
+          {menu.map((item) => {
+            return (
+              <Item
+                {...item}
+                key={item.id}
+                totalPrice={totalPrice}
+                changeItem={changeItem}
+                setTotalPrice={setTotalPrice}
+              />
+            );
+          })}
+        </div>
+
+        <Checkout>
+          <CheckoutItems>
+            <Wrapper>
+              <div style={styles.heading}>Items</div>
+              <div style={styles.heading}>Quantity</div>
+            </Wrapper>
+            {totalItem?.map((item) => {
+              return (
+                <Wrapper key={item.id}>
+                  <div>{item.itemName}</div>
+                  <div>{item.quantity}</div>
+                </Wrapper>
+              );
+            })}
+          </CheckoutItems>
+          <Button type="button" onClick={handlePay}>
+            Pay {totalPrice}
+          </Button>
+        </Checkout>
+      </Container>
+    </>
   );
 }
--- src/Item.tsx
+++ src/Item.tsx
@@ -1,38 +1,81 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
 
 interface Props {
   itemName: string;
   quantity: number;
+  price: number;
+  id: string;
   changeItem: any;
+  totalPrice: any;
+  setTotalPrice: any;
 }
 
-const Item: React.FC<Props> = (props) => {
-  const [itemName, setItemName] = useState<string>(props.itemName);
+const App = styled.div`
+  padding: 1em;
+  background: #fff;
+`;
 
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  padding: 1em;
+  justify-content: space-between;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Button = styled.div`
+  padding: 0.5em;
+  color: #000;
+  cursor: pointer;
+
+  &:hover {
+    background: #000;
+    color: #fff;
+  }
+`;
+
+const Item: React.FC<Props> = (props) => {
   const addItem = () => {
     console.log(props.quantity + 1);
     props.changeItem(props.itemName, props.quantity + 1);
+    props.setTotalPrice(props.totalPrice + props.price);
   };
 
   const removeItem = () => {
     if (props.quantity >= 1) {
       props.changeItem(props.itemName, props.quantity - 1);
+      props.setTotalPrice(props.totalPrice - props.price);
     } else {
       return null;
     }
   };
 
   return (
-    <div>
-      {props.itemName}
-      <button type="button" onClick={addItem}>
-        Add
-      </button>
-
-      <button type="button" onClick={removeItem}>
-        Remove
-      </button>
-    </div>
+    <App>
+      <Container>
+        <Wrapper>
+          <Button onClick={addItem}>
+            <i class="im im-plus"></i>
+          </Button>
+          <Button onClick={removeItem}>
+            <i class="im im-minus"></i>
+          </Button>
+          <div>{props.itemName}</div>
+        </Wrapper>
+        <div>
+          <div>{props.price}</div>
+        </div>
+      </Container>
+    </App>
   );
 };
 
